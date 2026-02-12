38b37c6831e768223f9c0331c35325b59e7c1b9b
Karan Chuenjaichum <47110342-k4lanakub@users.noreply.replit.com>
2026-02-12 10:21:22 +0000

    Hello World

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-import random
-def paoyingchub():
+#print hello world
+print("Hello World!")
 
-  score = 0
-  hands = ("hammer" , "scissors" , "paper")
+#print good bye
+print("Good Bye!")
 
-  while True:
-      user_hand = input("Choose Your hand: ").lower()
-      if user_hand == 'exit':
-        print(f"Your final score is: {score}. Good Bye")
-        break
-
-      if user_hand not in hands:
-        print("Your input is invalid, Choose again Only 'hammer', 'scissors', 'paper' ")
-        continue
-      com_hand = random.choice(hands)
-      print(f"Your opponent hand: {com_hand}")
-      if user_hand == com_hand:
-          print("Draw")
-      elif (user_hand == "hammer" and com_hand == "scissors") or \
-           (user_hand == "scissors" and com_hand == "paper") or \
-           (user_hand == "paper" and com_hand == "hammer"):
-          print("You Win")
-          score += 1
-      else:
-          score -= 1
-          print("You Lose")  
-
-      print(f"Your score is: {score}")  
-    
-      
-
-
-paoyingchub()
+#print new line
+print("i'm learning python")
