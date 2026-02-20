0b2532fd5931476f4db96cf0e3f8311ba15ba07d
k4lanakub <127275153+k4lanakub@users.noreply.github.com>
2026-02-20 08:06:46 +0700

    i knew it

--- main.py
+++ main.py
@@ -1,23 +1,2 @@
 #print hello world
-print("Hello World!")
-
-#print good bye
-print("Good Bye!")
-
-#print new line
-print("i'm learning python")
-
-
-
-def add_three(num):
-   return num + 3
-
-print(add_three(3), add_three(100))
-
-
-
-
-def add_four(num):
-   return num + 4
-
-print(add_four(4), add_three(100))
+print("EZ to know how to push or pull in github")
